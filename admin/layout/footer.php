bf44fdb45291e9c23136a24a6378da14395b9b73
xuananfpt <annxph37114@fpt.edu.vn>
2023-11-29 22:23:51 +0700

    Code Xuân An đẩy lên ngày 29/11

--- admin/layout/footer.php
+++ admin/layout/footer.php
@@ -1,6 +1,6 @@
 <div id="footer-wp" class="bg-danger">
     <div class="wp-inner">
-        <p id="copyright">2018 © Admin Theme by Php Master</p>
+        <p id="copyright">2018 © Admin Theme by DA STORE</p>
     </div>
 </div>
 </div>
